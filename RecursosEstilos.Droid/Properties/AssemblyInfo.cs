903d58eaa952bd8aadca9f4a6525e35151db3548
jgomezf <fogodaju@gmail.com>
2017-10-23 14:44:10 -0500

    Recursos y Estilos

--- RecursosEstilos.Droid/Properties/AssemblyInfo.cs
+++ RecursosEstilos.Droid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RecursosEstilos.Droid")]
+[assembly: AssemblyTitle("RecursosEstilos.Doid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RecursosEstilos.Droid")]
+[assembly: AssemblyProduct("RecursosEstilos.Doid")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
